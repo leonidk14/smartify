712b8b8550cf97e71e590c9868abbf9d9e845440
leonidk14 <leonid.kaida@outlook.com>
2026-06-14 21:06:45 +0200

    Send data to action

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,12 @@
+import type { Route } from "../+types/root";
 import { WordSearch } from "./wordSearch/wordSearch";
+
+export async function action({ request }: Route.ActionArgs) {
+  let formData = await request.formData();
+
+  console.log("formData", formData);
+  return "bla";
+}
 
 export default function Home() {
   return <WordSearch />;
--- app/routes/wordSearch/wordSearch.tsx
+++ app/routes/wordSearch/wordSearch.tsx
@@ -1,28 +1,38 @@
 import { ActionIcon, Flex, TextInput, useMantineTheme } from "@mantine/core";
-import { IconArrowRight, IconSearch } from "@tabler/icons-react";
+import { IconSearch } from "@tabler/icons-react";
+import { useState } from "react";
+import { useFetcher } from "react-router";
 
 export const WordSearch = () => {
+  const [isEditMode, setIsEditMode] = useState(true);
+  const fetcher = useFetcher();
   const theme = useMantineTheme();
 
   return (
     <Flex justify="center" direction="column" px={16} flex={1}>
-      <TextInput
-        radius="xl"
-        size="md"
-        placeholder="Which word to search for?"
-        rightSectionWidth={42}
-        rightSection={
-          <ActionIcon
-            size={32}
+      {isEditMode ? (
+        <fetcher.Form method="post">
+          <TextInput
             radius="xl"
-            color={theme.primaryColor}
-            variant="filled"
-            aria-label="Search">
-            <IconSearch size={18} stroke={1.5} />
-          </ActionIcon>
-        }
-        aria-label="Which word to search for?"
-      />
+            size="md"
+            placeholder="Which word to search for?"
+            name="search-item"
+            rightSectionWidth={42}
+            rightSection={
+              <ActionIcon
+                size={32}
+                radius="xl"
+                color={theme.primaryColor}
+                variant="filled"
+                aria-label="Search"
+                type="submit">
+                <IconSearch size={18} stroke={1.5} />
+              </ActionIcon>
+            }
+            aria-label="Which word to search for?"
+          />
+        </fetcher.Form>
+      ) : null}
     </Flex>
   );
 };
