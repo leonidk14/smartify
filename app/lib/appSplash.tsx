5b64572f052570fdd6e3f241edf69da8364022c1
leonidk14 <leonid.kaida@outlook.com>
2026-08-27 18:06:04 +0200

    Shift the icon

--- app/lib/appSplash.tsx
+++ app/lib/appSplash.tsx
@@ -1,28 +1,36 @@
-import { Flex, Text } from "@mantine/core";
+import { Box, Flex, Text } from "@mantine/core";
 import { text } from "../theme/typography";
 
 const MARK_SIZE = 256;
+const CAPTION_GAP = 28;
 
 export function AppSplash() {
   return (
     <Flex
-      direction="column"
       align="center"
       justify="center"
-      gap={28}
       flex={1}
       mih="100vh"
       bg="var(--color-surface)">
-      <img
-        className="app-splash-mark"
-        src="/pwa-512x512.png"
-        alt=""
-        width={MARK_SIZE}
-        height={MARK_SIZE}
-      />
-      <Text {...text.label} className="app-splash-caption">
-        Loading the vocabulary
-      </Text>
+      <Box pos="relative">
+        <img
+          className="app-splash-mark"
+          src="/pwa-512x512.png"
+          alt=""
+          width={MARK_SIZE}
+          height={MARK_SIZE}
+        />
+        <Text
+          {...text.label}
+          className="app-splash-caption"
+          pos="absolute"
+          top="100%"
+          left="50%"
+          mt={CAPTION_GAP}
+          style={{ whiteSpace: "nowrap" }}>
+          Loading the vocabulary
+        </Text>
+      </Box>
     </Flex>
   );
 }
